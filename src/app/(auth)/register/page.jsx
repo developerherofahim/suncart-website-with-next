8b42081f6da98e1f152b87ec3d73931dad09bf33
developerherofahim <developerherofahim@gmail.com>
2026-08-05 00:38:02 +0600

    fix google log in from register page

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -39,6 +39,19 @@ const RegisterPage = () => {
     console.log("Error:", error);
   }
 
+    const handleGoogleLogIn = async(data) => {
+    const { data: googleRes, error } = await authClient.signIn.social({
+      provider: "google",
+    });
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    console.log(data);
+  };
+
   return (
     <section className={`${poppins.className} flex flex-col md:flex-row gap-12`}>
       <div className="relative w-full md:w-1/2 min-h-screen overflow-hidden">
@@ -70,7 +83,7 @@ const RegisterPage = () => {
       <div className="py-12 w-full px-6">
         <h2 className="text-3xl font-bold mb-3 text-foreground">Create Account 🌊</h2>
         <p className="text-muted-foreground mb-8">Join SunCart and shop summer essentials</p>
-        <button className="w-full flex items-center justify-center gap-3 py-3 border border-border rounded-xl hover:bg-muted transition-colors mb-6 font-medium relative"><svg className="w-5 h-5" viewBox="0 0 24 24"><path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"></path><path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"></path><path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"></path><path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"></path></svg>Continue with Google</button>
+        <button onClick={handleGoogleLogIn} className="w-full flex items-center justify-center gap-3 py-3 border border-border rounded-xl hover:bg-muted transition-colors mb-6 font-medium relative"><svg className="w-5 h-5" viewBox="0 0 24 24"><path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"></path><path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"></path><path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"></path><path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"></path></svg>Continue with Google</button>
         <div className="relative flex items-center gap-3 mb-6">
           <div className="flex-1 h-px bg-foreground"></div>
           <span className="text-sm text-muted-foreground font-medium">or</span>
