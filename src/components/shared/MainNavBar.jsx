e17a645d68a9501b1b548d5391b697e274c4459f
developerherofahim <developerherofahim@gmail.com>
2026-08-04 22:06:04 +0600

    Resizing text in navbar

--- src/components/shared/MainNavBar.jsx
+++ src/components/shared/MainNavBar.jsx
@@ -39,7 +39,7 @@ export default function MainNavbar({ poppins }) {
       className={`${poppins} sticky top-0 z-50 border-b border-orange-100 bg-white/80 backdrop-blur-xl shadow-sm`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-15">
+        <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <Link href="/">
             <div>
@@ -94,7 +94,7 @@ export default function MainNavbar({ poppins }) {
               user ? (<Link
                 onClick={async () => await authClient.signOut()}
                 href="/login"
-                className="text-[#F77013] px-5 py-2 rounded-lg text-lg font-semibold hover:bg-orange-700 transition"
+                className="text-[#F77013] px-5 py-2 rounded-lg text-sm font-semibold hover:bg-orange-700 transition"
               >
                 Sign Out
               </Link>) : (<Link
